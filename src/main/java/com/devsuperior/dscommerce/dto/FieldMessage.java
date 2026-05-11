c8ea4ffd7a0312a974ff88159525e7378e8c105b
Jéssica Rodrigues <jessica.rodrigues@kaspper.com>
2026-05-11 12:15:05 -0300

    Bean Validation

---
+++ src/main/java/com/devsuperior/dscommerce/dto/FieldMessage.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class FieldMessage {
+
+    private String fieldName;
+    private String message;
+}
